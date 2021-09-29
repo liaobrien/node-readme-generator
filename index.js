3919267a79f2165e193777a5048cca252782871f
Lia OBrien <liaobrien123@gmail.com>
2021-09-29 09:20:10 -0400

    created an array to hold questions user needs to answer

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your GitHub username?", "What is your email address?", "What is your project's name?", "Please write a short description of your project.", "What kind of license should your project have?", "What command should be run to install dependencies?", "What does the user need to know about using the repo?", "What does the user need to know about contributing to the repo?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
