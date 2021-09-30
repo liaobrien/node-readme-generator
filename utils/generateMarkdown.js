5b66eeef8bf9efe0a76d5bc457a0d75de340ef6a
Lia OBrien <liaobrien123@gmail.com>
2021-09-30 15:26:20 -0400

    filled out renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![](https://img.shields.io/badge/license-${license}%20License-blue?style=flat-square)`;
+  }
+  else {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +20,8 @@ function renderLicenseSection(license) { }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
   return `# ${answers.title}
+
+${renderLicenseBadge(answers.license)}
 
 ## Description
 
