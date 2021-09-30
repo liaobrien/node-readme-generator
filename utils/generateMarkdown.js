3249edbe6f4a888b974a83ff456f8fea137bf40b
Lia OBrien <liaobrien123@gmail.com>
2021-09-30 18:09:19 -0400

    filled out renderLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,7 +22,16 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+    
+    This project is licensed under the ${license} license.`;
+  }
+  else {
+    return ``;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
@@ -54,9 +63,7 @@ To install necessary dependencies, run the following command:
 
 ${answers.usage}
 
-## License
-
-This project is licensed under the ${answers.license} license.
+${renderLicenseSection(answers.license)}
 
 ## Contributing
 
